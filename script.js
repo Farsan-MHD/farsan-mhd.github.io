f33661340814a2da613df804484956ecca54a9ec
M. J. M. FARSAN <mohammedfarsan603@gmail.com>
2025-10-14 18:40:49 +0300

    updated 14/10/2025       6:40pm

--- script.js
+++ script.js
@@ -94,15 +94,15 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Animate cards on scroll
-      document.addEventListener("DOMContentLoaded", function() {
-        const cards = document.querySelectorAll('.animate-card');
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach(entry => {
-            if(entry.isIntersecting) {
-              entry.target.classList.add('card-visible');
-              observer.unobserve(entry.target);
-            }
-          });
-        }, { threshold: 0.2 });
-        cards.forEach(card => observer.observe(card));
-      });
+document.addEventListener("DOMContentLoaded", function() {
+const cards = document.querySelectorAll('.animate-card');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+    if(entry.isIntersecting) {
+        entry.target.classList.add('card-visible');
+        observer.unobserve(entry.target);
+    }
+    });
+}, { threshold: 0.2 });
+cards.forEach(card => observer.observe(card));
+});
